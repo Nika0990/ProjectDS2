cb8614dc902e455b6c5fe2a995a18e0361933aba
Nika <veronika@campus.technion.ac.il>
2023-01-21 18:23:15 +0200

    HELP

--- Media_App/views.py
+++ Media_App/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.db import connection
+from .models import Programranks, Households, Programs, Recordorders,Recordreturns
 
 
 def dictfetchall(cursor):
@@ -19,10 +20,60 @@ def query_results(request):
                             GROUP BY g.genre, g.maxDuration
                             ORDER BY g.genre""")
         sql_res1 = dictfetchall(cursor)
-    return render(request, 'query_results.html', {'sql_res_1': sql_res1})
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT kp.title, Cast(AVG(Cast(pr.rank as FLOAT)) as DECIMAL(10,2)) as avgRank
+                            FROM EnoughKosherRanks kp, ProgramRanks pr
+                            WHERE kp.title = pr.title
+                            GROUP BY kp.title
+                            Order by avgRank desc, title""")
+        sql_res2 = dictfetchall(cursor)
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT title
+                            FROM relevantTitles rt
+                            WHERE    rt.title NOT IN
+                                    (SELECT title
+                                    FROM ProgramRanks pr
+                                    Where pr.rank < 2)
+                            ORDER BY title""")
+        sql_res3 = dictfetchall(cursor)
+    return render(request, 'query_results.html', {'sql_res_1': sql_res1, 'sql_res_2': sql_res2, 'sql_res_3': sql_res3})
 
 def rankings(request):
-    return render(request, 'rankings.html')
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT DISTINCT hID
+                            FROM Households""")
+        res1 = dictfetchall(cursor)
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT DISTINCT title
+                            FROM Programs""")
+        res2 = dictfetchall(cursor)
+    if request.method == 'POST' and request.POST:
+        id = request.POST["ID"]
+        title = request.POST["title"]
+        rank = request.POST["rank"]
+        with connection.cursor() as cursor:
+            cursor.execute("""SELECT *
+                                FROM ProgramRanks""")
+            ranked = dictfetchall(cursor)
+        for line in ranked:
+            if (line['hID'] == id) and (line['title'] == title) :
+                with connection.cursor() as cursor:
+                    cursor.execute("""DELETE 
+                                      FROM ProgramRanks
+                                      WHERE hID = %s and title = %s""",[id], [title])
+        with connection.cursor() as cursor:
+            cursor.execute("""SELECT *
+                                FROM Households
+                                WHERE hID = %s""",[id])
+            new_id = dictfetchall(cursor)
+        with connection.cursor() as cursor:
+            cursor.execute("""SELECT *
+                                FROM Programs
+                                WHERE title = %s""", [title])
+            new_title = dictfetchall(cursor)
+        new_Rank = Programranks(title =Programs(new_title)[0]['title'] ,hid=Households(new_id)[0]['hID'], rank = rank)
+        new_Rank.save()
+    return render(request, 'rankings.html', {'res_1': res1, 'res_2': res2})
 
 def records_management(request):
     return render(request, 'records_management.html')
