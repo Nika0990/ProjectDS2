a7878b27bbebd0adc85201ffffbe64ac6a5e4453
Nika <veronika@campus.technion.ac.il>
2023-01-22 19:51:52 +0200

    Rankings

--- Media_App/views.py
+++ Media_App/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db import connection
-from .models import Programranks, Households, Programs, Recordorders,Recordreturns
+from .models import Programranks, Households, Programs, Recordorders, Recordreturns
 
 
 def dictfetchall(cursor):
@@ -43,12 +43,25 @@ def rankings(request):
         cursor.execute("""SELECT DISTINCT hID
                             FROM Households""")
         res1 = dictfetchall(cursor)
+
     with connection.cursor() as cursor:
         cursor.execute("""SELECT DISTINCT title
                             FROM Programs""")
         res2 = dictfetchall(cursor)
-    if request.method == 'POST' and request.POST:
-        id = request.POST["ID"]
+
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT DISTINCT genre
+                            FROM(
+                                SELECT genre, count(title) as num_movies
+                                FROM Programs
+                                WHERE genre is not null
+                                Group by genre) as NumMovies
+                            WHERE num_movies >=5
+                            """)
+        res3 = dictfetchall(cursor)
+
+    if request.method == 'POST' and 'form1' in request.POST:
+        new_id = int(request.POST["ID"])
         title = request.POST["title"]
         rank = request.POST["rank"]
         with connection.cursor() as cursor:
@@ -56,24 +69,39 @@ def rankings(request):
                                 FROM ProgramRanks""")
             ranked = dictfetchall(cursor)
         for line in ranked:
-            if (line['hID'] == id) and (line['title'] == title) :
+            if (line['hID'] == new_id) and (line['title'] == title):
                 with connection.cursor() as cursor:
                     cursor.execute("""DELETE 
                                       FROM ProgramRanks
-                                      WHERE hID = %s and title = %s""",[id], [title])
+                                      WHERE hID = %s and title = %s""", [new_id, title])
+                break
         with connection.cursor() as cursor:
-            cursor.execute("""SELECT *
-                                FROM Households
-                                WHERE hID = %s""",[id])
-            new_id = dictfetchall(cursor)
+            cursor.execute("""INSERT INTO ProgramRanks
+                               VALUES (%s,%s,%s)""", [title, new_id, rank])
+
+    if request.method == 'POST' and 'form2' in request.POST:
+        new_genre = request.POST["genre"]
+        min_ranks = request.POST["min_ranks"]
         with connection.cursor() as cursor:
-            cursor.execute("""SELECT *
-                                FROM Programs
-                                WHERE title = %s""", [title])
-            new_title = dictfetchall(cursor)
-        new_Rank = Programranks(title =Programs(new_title)[0]['title'] ,hid=Households(new_id)[0]['hID'], rank = rank)
-        new_Rank.save()
-    return render(request, 'rankings.html', {'res_1': res1, 'res_2': res2})
+            cursor.execute("""SELECT arr.title, avgRank
+                                FROM avgRelevantRanks arr, Programs p, rankingsCount rc
+                                Where arr.title = p.title and p.genre = %s and rc.title = p.title and rc.rankings >= %s
+                                ORDER BY avgRank desc, title""", [new_genre, min_ranks])
+            temp = dictfetchall(cursor)
+        with connection.cursor() as cursor:
+            cursor.execute("""SELECT rz.title, 0.00 as avgRank
+                                FROM rankZeros rz, Programs p
+                                WHERE rz.title = p.title and p.genre = %s
+                                    union
+                                SELECT arr.title,0.00 as avgRank
+                                FROM avgRelevantRanks arr, Programs p, rankingsCount rc
+                                Where arr.title = p.title and p.genre = %s and rc.title = p.title and rc.rankings < %s
+                            ORDER BY title""", [new_genre, new_genre, min_ranks])
+            temp.extend(dictfetchall(cursor))
+        res4 = temp[0:5]
+        return render(request, 'rankings.html', {'res_1': res1, 'res_2': res2, 'res_3': res3, 'res_4': res4})
+
+    return render(request, 'rankings.html', {'res_1': res1, 'res_2': res2, 'res_3': res3})
 
 def records_management(request):
     return render(request, 'records_management.html')
